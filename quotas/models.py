31f89731c04d543aefbef01a25bafae18b5c067f
Simon Guilbault <simon.guilbault@calculquebec.ca>
2021-03-19 19:04:30 +0000

    Adding project quota breakdown

--- quotas/models.py
+++ quotas/models.py
@@ -2,6 +2,7 @@ from django.db import models
 import ldapdb.models
 from ldapdb.models import fields
 from django.conf import settings
+from django.utils.encoding import smart_str
 
 class LdapAllocation(ldapdb.models.Model):
     base_dn = 'dc=computecanada,dc=local'
@@ -32,3 +33,54 @@ class LdapAllocation(ldapdb.models.Model):
                     quota_info[key] = float(value)
                 resources.append(quota_info)
         return resources
+
+class AcctStat(models.Model):
+    class FileType(models.TextChoices):
+        SYMLINK = 'symlink'
+        DIR = 'dir'
+        FILE = 'file'
+        CHR = 'chr'
+        BLK = 'blk'
+        FIFO = 'fifo'
+        SOCK = 'sock'
+
+    class LhsmStatus(models.TextChoices):
+        NONE = ''
+        NEW = 'new'
+        MODIFIED = 'modified'
+        RETRIEVING = 'retrieving'
+        ARCHIVING = 'archiving'
+        SYNCHRO = 'synchro'
+        RELEASED = 'released'
+
+    uid = models.CharField(primary_key=True, max_length=127)
+    gid = models.CharField(max_length=127)
+    type = models.CharField(
+        max_length=7,
+        choices = FileType.choices,
+    )
+    lhsm_status = models.CharField(
+        max_length=10,
+        choices=LhsmStatus.choices,
+    )
+    size = models.PositiveBigIntegerField(blank=True, null=True)
+    blocks = models.PositiveBigIntegerField(blank=True, null=True)
+    count = models.PositiveBigIntegerField(blank=True, null=True)
+    sz0 = models.PositiveBigIntegerField(blank=True, null=True)
+    sz1 = models.PositiveBigIntegerField(blank=True, null=True)
+    sz32 = models.PositiveBigIntegerField(blank=True, null=True)
+    sz1k = models.PositiveBigIntegerField(db_column='sz1K', blank=True, null=True)  # Field name made lowercase.
+    sz32k = models.PositiveBigIntegerField(db_column='sz32K', blank=True, null=True)  # Field name made lowercase.
+    sz1m = models.PositiveBigIntegerField(db_column='sz1M', blank=True, null=True)  # Field name made lowercase.
+    sz32m = models.PositiveBigIntegerField(db_column='sz32M', blank=True, null=True)  # Field name made lowercase.
+    sz1g = models.PositiveBigIntegerField(db_column='sz1G', blank=True, null=True)  # Field name made lowercase.
+    sz32g = models.PositiveBigIntegerField(db_column='sz32G', blank=True, null=True)  # Field name made lowercase.
+    sz1t = models.PositiveBigIntegerField(db_column='sz1T', blank=True, null=True)  # Field name made lowercase.
+
+    def uid_string(self):
+        return smart_str(self.uid)
+
+    class Meta:
+        managed = False
+        db_table = 'ACCT_STAT'
+        unique_together = (('uid', 'gid', 'type', 'lhsm_status'),)
--- quotas/views.py
+++ quotas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import LdapAllocation
+from .models import LdapAllocation, AcctStat
+from django.db.models import Q, Avg, Count, Min, Sum
 
 def index(request):
     context = {}
@@ -26,6 +27,60 @@ def index(request):
             if 'nearline_storage_tb' in resource:
                 nearline['name'] = alloc.name
                 nearline['nearline_storage_tb'] = resource['nearline_storage_tb']
+
+        project_user_quota = AcctStat.objects.using('rbh-lustre03')\
+.filter(gid=alloc.name)\
+.exclude(uid='root')\
+.filter(Q(lhsm_status='') | Q(lhsm_status='new'))\
+.annotate(Sum('count'))\
+.annotate(Sum('blocks'))\
+.filter(count__sum__gt=2)\
+.order_by('-blocks__sum')\
+.all()
+        prepared_user_quota = []
+        for item in project_user_quota.iterator():
+            prepared_user_quota.append({
+                'uid_string': item.uid_string,
+                'count__sum': item.count__sum,
+                'bytes__sum': item.blocks__sum * 512
+            })
+        project['users'] = prepared_user_quota
+
+        project_used = AcctStat.objects.using('rbh-lustre03')\
+.all()\
+.filter(gid=alloc.name)\
+.filter(Q(lhsm_status='') | Q(lhsm_status='new'))\
+.aggregate(count=Sum('count'), blocks=Sum('blocks'))
+
+        nearline_on_disk = AcctStat.objects.using('rbh-lustre03')\
+.all()\
+.filter(gid=alloc.name)\
+.exclude(lhsm_status='')\
+.exclude(lhsm_status='new')\
+.exclude(lhsm_status='released')\
+.aggregate(count=Sum('count'), blocks=Sum('blocks'))
+        nearline_released = AcctStat.objects.using('rbh-lustre03')\
+.all()\
+.filter(gid=alloc.name)\
+.exclude(lhsm_status='')\
+.exclude(lhsm_status='new')\
+.exclude(lhsm_status='released')\
+.aggregate(count=Sum('count'), size=Sum('size'))
+
+        if project_used['count'] is None or project_used['blocks'] is None:
+            project['used'] = {
+                'inodes': 0,
+                'TBs': 0,
+                'bytes_ratio': 0.0,
+                'inode_ratio': 0.0,
+            }
+        else:
+            project['used'] = {
+                'inodes': project_used['count'],
+                'TBs': project_used['blocks'] * 512 / 1024 / 1024 / 1024 / 1024,
+                'bytes_ratio': project_used['blocks'] * 512 / (project['project_storage_tb'] * 1024 * 1024 * 1024 * 1024) * 100,
+                'inodes_ratio':  project_used['count'] / (project['inode_quota'] * 1000000) * 100,
+            }
 
         context['projects'].append(project)
         context['nearlines'].append(nearline)

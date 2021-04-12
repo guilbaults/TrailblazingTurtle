2453bdefe8bd98af150e2996072b98f245891f04
Simon Guilbault <sigui4@userportal.int.ets1.calculquebec.ca>
2021-04-12 14:46:15 +0000

    Moving prometheus in common module

--- jobstats/views.py
+++ jobstats/views.py
@@ -3,43 +3,13 @@ from django.http import HttpResponseNotFound, JsonResponse
 from slurm.models import JobTable
 from ccldap.models import LdapUser
 from userportal.common import user_or_staff
+from userportal.common import Prometheus
 import time
 from django.conf import settings
-from prometheus_api_client import PrometheusConnect
 from datetime import datetime, timedelta
 from django.contrib.auth.decorators import login_required
 from django.utils.translation import gettext as _
 import statistics
-
-
-class Prometheus:
-    def __init__(self, url):
-        self.url = url
-        self.prom = PrometheusConnect(url=url, disable_ssl=True)
-
-    def query_prometheus(self, query, duration, step):
-        values = self.query_prometheus_multiple(query, duration, step)
-        if len(values) == 0:
-            raise ValueError
-        return (values[0]['x'], values[0]['y'])
-
-    def query_prometheus_multiple(self, query, start, end=None):
-        if end is None:
-            end = datetime.now()
-        q = self.prom.custom_query_range(
-            query=query,
-            start_time=start,
-            end_time=end,
-            step='3m',
-        )
-        return_list = []
-        for line in q:
-            return_list.append({
-                'metric': line['metric'],
-                'x': [datetime.fromtimestamp(x[0]) for x in line['values']],
-                'y': [float(x[1]) for x in line['values']]
-            })
-        return return_list
 
 
 @login_required
--- userportal/common.py
+++ userportal/common.py
@@ -1,5 +1,7 @@
 import functools
 from django.http import HttpResponseNotFound
+from prometheus_api_client import PrometheusConnect
+from datetime import datetime, timedelta
 
 
 def user_or_staff(func):
@@ -14,3 +16,37 @@ def user_or_staff(func):
         else:
             return HttpResponseNotFound()
     return wrapper
+
+
+class Prometheus:
+    def __init__(self, url):
+        self.url = url
+        self.prom = PrometheusConnect(url=url, disable_ssl=True)
+
+    def query_prometheus(self, query, duration, step):
+        values = self.query_prometheus_multiple(query, duration, step)
+        if len(values) == 0:
+            raise ValueError
+        return (values[0]['x'], values[0]['y'])
+
+    def query_prometheus_multiple(self, query, start, end=None):
+        if end is None:
+            end = datetime.now()
+        q = self.prom.custom_query_range(
+            query=query,
+            start_time=start,
+            end_time=end,
+            step='3m',
+        )
+        return_list = []
+        for line in q:
+            return_list.append({
+                'metric': line['metric'],
+                'x': [datetime.fromtimestamp(x[0]) for x in line['values']],
+                'y': [float(x[1]) for x in line['values']]
+            })
+        return return_list
+
+    def query_last(self, query):
+        q = self.prom.custom_query(query)
+        return q

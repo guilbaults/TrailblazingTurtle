023cccabad320706bebe4150686da7a0737416d6
Simon Guilbault <sigui4@userportal.int.ets1.calculquebec.ca>
2021-04-13 20:39:25 +0000

    Adding a largemem page

--- top/urls.py
+++ top/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('compute/', views.compute),
+    path('largemem/', views.largemem),
 ]
--- top/views.py
+++ top/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from userportal.common import staff
 from userportal.common import Prometheus
+from slurm.models import JobTable
+from ccldap.models import LdapUser
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+import time
 
 
 @login_required
@@ -40,3 +43,50 @@ def compute(request):
 
     context['cpu_users'] = cpu_users
     return render(request, 'top/compute.html', context)
+
+
+@login_required
+@staff
+def largemem(request):
+    context = {}
+    prom = Prometheus(settings.PROMETHEUS['url'])
+
+    hour_ago = int(time.time()) - (3600)
+    week_ago = int(time.time()) - (3600 * 24 * 7)
+
+    jobs_running = JobTable.objects.filter(
+        time_start__gt=hour_ago,
+        time_eligible__gt=week_ago,  # until time_start is indexed...
+        partition__icontains='large',
+        state=1).order_by('-time_submit')
+
+    jobs = []
+    for job in jobs_running:
+        try:
+            query_cpu_asked = 'count(slurm_job_core_usage_total{{slurmjobid="{}"}})'.format(job.id_job)
+            stats_cpu_asked = prom.query_last(query_cpu_asked)
+            query_cpu_used = 'sum(rate(slurm_job_core_usage_total{{slurmjobid="{}"}}[2m]) / 1000000000)'.format(job.id_job)
+            stats_cpu_used = prom.query_last(query_cpu_used)
+
+            query_mem_asked = 'sum(slurm_job_memory_limit{{slurmjobid="{}"}})'.format(job.id_job)
+            stats_mem_asked = prom.query_last(query_mem_asked)
+            query_mem_max = 'sum(slurm_job_memory_max{{slurmjobid="{}"}})'.format(job.id_job)
+            stats_mem_max = prom.query_last(query_mem_max)
+
+            jobs.append({
+                'user': LdapUser.objects.filter(uid=job.id_user).get().username,
+                'job_id': job.id_job,
+                'cpu_asked': stats_cpu_asked[0]['value'][1],
+                'cpu_used': stats_cpu_used[0]['value'][1],
+                'mem_asked': stats_mem_asked[0]['value'][1],
+                'mem_max': stats_mem_max[0]['value'][1],
+            })
+        except IndexError:
+            jobs.append({
+                'user': LdapUser.objects.filter(uid=job.id_user).get().username,
+                'job_id': job.id_job,
+                'mem_asked': 'error',
+                'mem_max': 'error',
+            })
+        context['jobs'] = jobs
+    return render(request, 'top/largemem.html', context)

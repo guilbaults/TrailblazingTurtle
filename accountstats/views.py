72ccf5c1562fa122563f49519597342e9a74c09f
Simon Guilbault <sigui4@userportal.int.ets1.calculquebec.ca>
2021-04-15 13:57:43 +0000

    Adding memory for accountstats

--- accountstats/views.py
+++ accountstats/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseNotFound, JsonResponse
-from slurm.models import JobTable
-from ccldap.models import LdapUser
+from django.http import JsonResponse
 from userportal.common import staff
 from userportal.common import Prometheus
-import time
 from django.conf import settings
 from datetime import datetime, timedelta
 from django.contrib.auth.decorators import login_required
-from django.utils.translation import gettext as _
-import statistics
 
 
 @login_required
@@ -22,6 +17,30 @@ def index(request):
 def account(request, account):
     context = {}
     return render(request, 'accountstats/account.html', context)
+
+
+@login_required
+@staff
+def graph_cpu_allocated(request, account):
+    prom = Prometheus(settings.PROMETHEUS['url'])
+    data = {'lines': []}
+    query_alloc = 'count(slurm_job_core_usage_total{{account="{}"}}) by (user)'.format(account)
+    stats_alloc = prom.query_prometheus_multiple(query_alloc, datetime.now() - timedelta(hours=6), datetime.now())
+
+    for line in stats_alloc:
+        user = line['metric']['user']
+        x = list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), line['x']))
+        y = line['y']
+        data['lines'].append({
+            'x': x,
+            'y': y,
+            'type': 'scatter',
+            'stackgroup': 'one',
+            'name': user
+        })
+
+    return JsonResponse(data)
+
 
 @login_required
 @staff
@@ -45,30 +64,13 @@ def graph_cpu_used(request, account):
 
     return JsonResponse(data)
 
-def graph_cpu_allocated(request, account):
-    prom = Prometheus(settings.PROMETHEUS['url'])
-    data = {'lines': []}
-    query_alloc = 'count(slurm_job_core_usage_total{{account="{}"}}) by (user)/ 1000000000'.format(account)
-    stats_alloc = prom.query_prometheus_multiple(query_alloc, datetime.now() - timedelta(hours=6), datetime.now())
 
-    for line in stats_alloc:
-        user = line['metric']['user']
-        x = list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), line['x']))
-        y = line['y']
-        data['lines'].append({
-            'x': x,
-            'y': y,
-            'type': 'scatter',
-            'stackgroup': 'one',
-            'name': user
-        })
-
-    return JsonResponse(data)
-
+@login_required
+@staff
 def graph_cpu_wasted(request, account):
     prom = Prometheus(settings.PROMETHEUS['url'])
     data = {'lines': []}
-    query_alloc = 'count(slurm_job_core_usage_total{{account="{}"}}) by (user) - sum(rate(slurm_job_core_usage_total{{account="{}"}}[5m])) by (user)/ 1000000000'.format(account, account)
+    query_alloc = 'clamp_min(count(slurm_job_core_usage_total{{account="{}"}}) by (user) - sum(rate(slurm_job_core_usage_total{{account="{}"}}[5m])) by (user)/ 1000000000, 0)'.format(account, account)
     stats_alloc = prom.query_prometheus_multiple(query_alloc, datetime.now() - timedelta(hours=6), datetime.now())
 
     for line in stats_alloc:
@@ -85,3 +87,89 @@ def graph_cpu_wasted(request, account):
 
     return JsonResponse(data)
 
+
+@login_required
+@staff
+def graph_mem_allocated(request, account):
+    prom = Prometheus(settings.PROMETHEUS['url'])
+    data = {'lines': []}
+    query_alloc = 'sum(slurm_job_memory_limit{{account="{}"}}) by (user) /(1024*1024*1024)'.format(account)
+    stats_alloc = prom.query_prometheus_multiple(query_alloc, datetime.now() - timedelta(hours=6), datetime.now())
+
+    for line in stats_alloc:
+        user = line['metric']['user']
+        x = list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), line['x']))
+        y = line['y']
+        data['lines'].append({
+            'x': x,
+            'y': y,
+            'type': 'scatter',
+            'stackgroup': 'one',
+            'name': user
+        })
+
+    data['layout'] = {
+        'yaxis': {
+            'ticksuffix': 'GiB',
+        }
+    }
+
+    return JsonResponse(data)
+
+
+@login_required
+@staff
+def graph_mem_used(request, account):
+    prom = Prometheus(settings.PROMETHEUS['url'])
+    data = {'lines': []}
+    query_used = 'sum(slurm_job_memory_max{{account="{}"}}) by (user) /(1024*1024*1024)'.format(account)
+    stats_used = prom.query_prometheus_multiple(query_used, datetime.now() - timedelta(hours=6), datetime.now())
+
+    for line in stats_used:
+        user = line['metric']['user']
+        x = list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), line['x']))
+        y = line['y']
+        data['lines'].append({
+            'x': x,
+            'y': y,
+            'type': 'scatter',
+            'stackgroup': 'one',
+            'name': user,
+        })
+
+    data['layout'] = {
+        'yaxis': {
+            'ticksuffix': 'GiB',
+        }
+    }
+
+    return JsonResponse(data)
+
+
+@login_required
+@staff
+def graph_mem_wasted(request, account):
+    prom = Prometheus(settings.PROMETHEUS['url'])
+    data = {'lines': []}
+    query_alloc = 'clamp_min(sum(slurm_job_memory_limit{{account="{}"}}) by (user) - sum(slurm_job_memory_max{{account="{}"}}) by (user), 0) /(1024*1024*1024)'.format(account, account)
+    stats_alloc = prom.query_prometheus_multiple(query_alloc, datetime.now() - timedelta(hours=6), datetime.now())
+
+    for line in stats_alloc:
+        user = line['metric']['user']
+        x = list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), line['x']))
+        y = line['y']
+        data['lines'].append({
+            'x': x,
+            'y': y,
+            'type': 'scatter',
+            'stackgroup': 'one',
+            'name': user
+        })
+
+    data['layout'] = {
+        'yaxis': {
+            'ticksuffix': 'GiB',
+        }
+    }
+
+    return JsonResponse(data)

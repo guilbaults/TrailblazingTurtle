7205cc3ef6c7b901e9d71b5ba14a412d163f8c42
Simon Guilbault <sigui4@userportal.int.ets1.calculquebec.ca>
2021-04-15 15:12:01 +0000

    Adding wasted GPUs per account

--- accountstats/views.py
+++ accountstats/views.py
@@ -173,3 +173,131 @@ def graph_mem_wasted(request, account):
     }
 
     return JsonResponse(data)
+
+
+@login_required
+@staff
+def graph_lustre_mdt(request, account):
+    prom = Prometheus(settings.PROMETHEUS['url'])
+
+    query = 'sum(rate(lustre_job_stats_total{{component=~"mdt",account=~"{}"}}[5m])) by (user, fs) !=0'.format(account)
+    stats = prom.query_prometheus_multiple(query, datetime.now() - timedelta(hours=6), datetime.now())
+    data = {'lines': []}
+    for line in stats:
+        user = line['metric']['user']
+        fs = line['metric']['fs']
+        x = list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), line['x']))
+        y = line['y']
+        data['lines'].append({
+            'x': x,
+            'y': y,
+            'type': 'scatter',
+            'stackgroup': 'one',
+            'name': '{} {}'.format(user, fs)
+        })
+
+    data['layout'] = {
+        'yaxis': {
+            'ticksuffix': ' IOPS'
+        }
+    }
+    return JsonResponse(data)
+
+
+@login_required
+@staff
+def graph_lustre_ost(request, account):
+    prom = Prometheus(settings.PROMETHEUS['url'])
+    data = {'lines': []}
+    for i in ['read', 'write']:
+        query = '(sum(rate(lustre_job_{}_bytes_total{{component=~"ost",account=~"{}",target=~".*-OST.*"}}[5m])) by (user, fs)) / (1024*1024)'.format(i, account)
+        stats = prom.query_prometheus_multiple(query, datetime.now() - timedelta(hours=6), datetime.now())
+
+        for line in stats:
+            fs = line['metric']['fs']
+            user = line['metric']['user']
+            x = list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), line['x']))
+            y = line['y']
+            data['lines'].append({
+                'x': x,
+                'y': y,
+                'type': 'scatter',
+                'stackgroup': 'one',
+                'name': '{} {} {}'.format(i, fs, user)
+            })
+
+    data['layout'] = {
+        'yaxis': {
+            'ticksuffix': ' MiB/s'
+        }
+    }
+    return JsonResponse(data)
+
+
+@login_required
+@staff
+def graph_gpu_allocated(request, account):
+    prom = Prometheus(settings.PROMETHEUS['url'])
+
+    query = 'count(slurm_job_utilization_gpu{{account="{}"}}) by (user)'.format(account)
+    stats = prom.query_prometheus_multiple(query, datetime.now() - timedelta(hours=6), datetime.now())
+
+    data = {'lines': []}
+    for line in stats:
+        x = list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), line['x']))
+        y = line['y']
+        data['lines'].append({
+            'x': x,
+            'y': y,
+            'type': 'scatter',
+            'stackgroup': 'one',
+            'name': line['metric']['user']
+        })
+
+    return JsonResponse(data)
+
+
+@login_required
+@staff
+def graph_gpu_used(request, account):
+    prom = Prometheus(settings.PROMETHEUS['url'])
+
+    query = 'sum(slurm_job_utilization_gpu{{account="{}"}}) by (user) / 100'.format(account)
+    stats = prom.query_prometheus_multiple(query, datetime.now() - timedelta(hours=6), datetime.now())
+
+    data = {'lines': []}
+    for line in stats:
+        x = list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), line['x']))
+        y = line['y']
+        data['lines'].append({
+            'x': x,
+            'y': y,
+            'type': 'scatter',
+            'stackgroup': 'one',
+            'name': line['metric']['user']
+        })
+
+    return JsonResponse(data)
+
+
+@login_required
+@staff
+def graph_gpu_wasted(request, account):
+    prom = Prometheus(settings.PROMETHEUS['url'])
+
+    query = 'count(slurm_job_utilization_gpu{{account="{}"}}) by (user) - sum(slurm_job_utilization_gpu{{account="{}"}}) by (user) / 100'.format(account, account)
+    stats = prom.query_prometheus_multiple(query, datetime.now() - timedelta(hours=6), datetime.now())
+
+    data = {'lines': []}
+    for line in stats:
+        x = list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), line['x']))
+        y = line['y']
+        data['lines'].append({
+            'x': x,
+            'y': y,
+            'type': 'scatter',
+            'stackgroup': 'one',
+            'name': line['metric']['user']
+        })
+
+    return JsonResponse(data)

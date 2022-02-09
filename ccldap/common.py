1869d7ac1efa097046418de4210c38c90e04ac4d
Simon Guilbault <sigui4@userportal101.narval.calcul.quebec>
2022-02-09 18:53:02 +0000

    Completing the usersummary page

--- ccldap/common.py
+++ ccldap/common.py
@@ -9,7 +9,7 @@ def storage_allocations(username):
         resources = alloc.parse_active_resources()
         project = {
             'project_storage_tb': 1.0,
-            'inode_quota': 1.0,
+            'inodes_quota': int(1.0 * 1000 * 1000), # specified in millions in ldap
         }
         nearline = {
             'nearline_storage_tb': 0.0,
@@ -19,8 +19,9 @@ def storage_allocations(username):
                 project['name'] = alloc.name
                 if 'project_storage_tb' in resource:
                     project['project_storage_tb'] = resource['project_storage_tb']
+                    project['project_storage_bytes'] = int(project['project_storage_tb'] * 1024 * 1024 * 1024 * 1024)
                 if 'inode_quota' in resource:
-                    project['inode_quota'] = resource['inode_quota']
+                    project['inodes_quota'] = int(resource['inode_quota'] * 1000 * 1000) # convert millions to integers
                 projects.append(project)
             if 'nearline_storage_tb' in resource:
                 nearline['name'] = alloc.name
--- usersummary/views.py
+++ usersummary/views.py
@@ -3,6 +3,9 @@ from ccldap.models import LdapUser
 from userportal.common import user_or_staff
 from django.contrib.auth.decorators import login_required
 from slurm.models import JobTable
+from ccldap.common import storage_allocations
+from userportal.common import Prometheus
+from django.conf import settings
 
 
 @login_required
@@ -10,11 +13,55 @@ def index(request):
     return redirect('{}/'.format(request.user.username.split('@')[0]))
 
 
+def get_quota_prometheus(quota_name, quota_type, inodes_bytes='inodes'):
+    prom = Prometheus(settings.PROMETHEUS)
+    if inodes_bytes == 'inodes':
+        metric_name = 'lfs_quota_inodes'
+    else:
+        metric_name = 'lfs_quota_kbytes'
+
+    try:
+        query_quota = "{}{{path='{}', {}='{}', {} }}".format(
+            metric_name,
+            settings.QUOTA_TYPES[quota_type][0],
+            settings.QUOTA_TYPES[quota_type][1],
+            quota_name,
+            prom.get_filter())
+        stats_quota = prom.query_last(query_quota)
+        if inodes_bytes == 'inodes':
+            return int(stats_quota[0]['value'][1])
+        else:
+            return int(stats_quota[0]['value'][1]) * 1024 # return it in bytes
+    except IndexError:
+        return None
+
+
 @login_required
 @user_or_staff
 def user(request, username):
     uid = LdapUser.objects.filter(username=username).get().uid
     context = {'username': username}
+
+    context['scratch'] = {
+        'inodes_quota': 10000000,
+        'bytes_quota': 20 * 1024 * 1024 * 1024 * 1024,
+        'inodes': get_quota_prometheus(username, 'scratch', 'inodes'),
+        'bytes': get_quota_prometheus(username, 'scratch', 'bytes'),
+    }
+    context['home'] = {
+        'inodes_quota': 500000,
+        'bytes_quota': 50 * 1024 * 1024 * 1024,
+        'inodes': get_quota_prometheus(username, 'home', 'inodes'),
+        'bytes': get_quota_prometheus(username, 'home', 'bytes'),
+    }
+
+    alloc_projects, alloc_nearlines = storage_allocations(username)
+    for project in alloc_projects:
+        project['inodes'] = get_quota_prometheus(project['name'], 'project', 'inodes')
+        project['bytes'] = get_quota_prometheus(project['name'], 'project', 'bytes')
+    context['alloc_projects'] = alloc_projects
+    context['alloc_nearlines'] = alloc_nearlines
+
     pending_jobs = JobTable.objects.filter(
         id_user=uid, state=0).order_by('-time_submit')
 

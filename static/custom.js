ad9a85b73f1043cce33f09521e17beeabefdc1ba
Simon Guilbault <sigui4@userportal101.narval.calcul.quebec>
2022-03-29 19:54:17 +0000

    Adding a message when there is no data in the graph

--- static/custom.js
+++ static/custom.js
@@ -2,6 +2,10 @@ function replace_div_alert(container_div){
     $(container_div).html('<div class="alert alert-warning" role="alert">Could not load this graph</div>');
 }
 
+function replace_div_nodata(container_div){
+    $(container_div).html('<div class="alert alert-primary" role="alert">No data</div>');
+}
+
 function loadGraph(container, url){
     var container_div = '#' + container;
     $(container_div).html('<div class="spinner-border m-5 justify-content-center" role="status"><span class="sr-only">Loading...</span></div>');
@@ -14,11 +18,15 @@ function loadGraph(container, url){
         dataType    : 'json',
         contentType : 'application/json',
         success : function(data) {
-            $(container_div).html('');
-            Plotly.newPlot(container, data['lines'], data['layout']);
+            if(data['lines'].length == 0){
+                replace_div_nodata(container_div);
+            }
+            else{
+                $(container_div).html('');
+                Plotly.newPlot(container, data['lines'], data['layout']);
+            }
         },
         error : function(xhr, textStatus, errorThrown ) {
-            console.log(textStatus);
             if (textStatus == 'timeout' || textStatus == 'error') {
                 this.tryCount++;
                 if (this.tryCount <= this.retryLimit) {

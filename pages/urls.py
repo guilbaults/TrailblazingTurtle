ac4829de72fb79c298c34bf10af9607f3e4fb230
Simon Guilbault <sigui4@userportal101.narval.calcul.quebec>
2022-05-06 18:44:00 +0000

    Adding login nodes stats

--- pages/urls.py
+++ pages/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('filesystem/', views.filesystem),
     path('filesystem/graph/lustre/<str:fs>/mdt.json', views.graph_lustre_mdt),
     path('filesystem/graph/lustre/<str:fs>/ost.json', views.graph_lustre_ost),
+    path('logins/', views.logins),
+    path('logins/graph/cpu/<str:login>.json', views.graph_login_cpu),
+    path('logins/graph/memory/<str:login>.json', views.graph_login_memory),
+    path('logins/graph/load/<str:login>.json', views.graph_login_load),
+    path('logins/graph/network/<str:login>.json', views.graph_login_network),
 ]
--- pages/views.py
+++ pages/views.py
@@ -18,6 +18,12 @@ def filesystem(request):
     return render(request, 'pages/filesystem.html', context)
 
 
+def logins(request):
+    context = {}
+    context['logins'] = settings.LOGINS.keys()
+    return render(request, 'pages/logins.html', context)
+
+
 def query_time(request):
     delta = request.GET.get('delta', '1h')
 
@@ -46,11 +52,9 @@ def graph_lustre_mdt(request, fs):
     data = {'lines': []}
     for line in stats:
         operation = line['metric']['operation']
-        x = list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), line['x']))
-        y = line['y']
         data['lines'].append({
-            'x': x,
-            'y': y,
+            'x': list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), line['x'])),
+            'y': line['y'],
             'type': 'scatter',
             'stackgroup': 'one',
             'name': '{}'.format(operation)
@@ -83,11 +87,9 @@ def graph_lustre_ost(request, fs):
         query = 'lustre:{}_bytes:rate3m{{fs="{}", {}}}'.format(i, fs, prom.get_filter())
         stats = prom.query_prometheus_multiple(query, timing[0], step=timing[1])
         for line in stats:
-            x = list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), line['x']))
-            y = line['y']
             data['lines'].append({
-                'x': x,
-                'y': y,
+                'x': list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), line['x'])),
+                'y': line['y'],
                 'type': 'scatter',
                 'name': '{}'.format(i)
             })
@@ -107,4 +109,179 @@ def graph_lustre_ost(request, fs):
         },
         'height': 300,
     }
+    return JsonResponse(data)
+
+
+def graph_login_cpu(request, login):
+    if login not in settings.LOGINS.keys():
+        return JsonResponse({'error': 'Unknown login node'})
+    timing = query_time(request)
+
+    core_count_query = 'count(node_cpu_seconds_total{{mode="system",instance=~"{login}:.*", {filter} }})'.format(
+        login=login,
+        filter=prom.get_filter(),
+    )
+    core_count = max(prom.query_prometheus(core_count_query, timing[0], step=timing[1])[1])
+    data = {'lines': []}
+    query = 'sum by (mode)(rate(node_cpu_seconds_total{{mode=~"system|user|iowait",instance=~"{login}:.*", {filter} }}[{step}]))'.format(
+        login=login,
+        filter=prom.get_filter(),
+        step=timing[1],
+    )
+    stats = prom.query_prometheus_multiple(query, timing[0], step=timing[1])
+
+    for line in stats:
+        data['lines'].append({
+            'x': list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), line['x'])),
+            'y': line['y'],
+            'type': 'scatter',
+            'name': '{}'.format(line['metric']['mode']),
+        })
+
+    data['layout'] = {
+        'yaxis': {
+            'ticksuffix': 'Cores',
+            'tickformat': '~s',
+            'range': [0, core_count],
+        },
+        'showlegend': False,
+        'margin': {
+            'l': 70,
+            'r': 0,
+            'b': 50,
+            't': 0,
+            'pad': 0
+        },
+        'height': 300,
+    }
+
+    return JsonResponse(data)
+
+
+def graph_login_memory(request, login):
+    if login not in settings.LOGINS.keys():
+        return JsonResponse({'error': 'Unknown login node'})
+    timing = query_time(request)
+
+    total_mem_query = 'node_memory_MemTotal_bytes{{instance=~"{login}:.*", {filter} }}'.format(
+        login=login,
+        filter=prom.get_filter(),
+    )
+    total_mem = max(prom.query_prometheus(total_mem_query, timing[0], step=timing[1])[1])
+    data = {'lines': []}
+    query = 'node_memory_MemTotal_bytes{{instance=~"{login}:.*", {filter} }} - node_memory_MemFree_bytes{{instance=~"{login}:.*", {filter} }} - node_memory_Buffers_bytes{{instance=~"{login}:.*", {filter} }} - node_memory_Cached_bytes{{instance=~"{login}:.*", {filter} }} - node_memory_Slab_bytes{{instance=~"{login}:.*", {filter} }} - node_memory_PageTables_bytes{{instance=~"{login}:.*", {filter} }} - node_memory_SwapCached_bytes{{instance=~"{login}:.*", {filter} }}'.format(
+        login=login,
+        filter=prom.get_filter(),
+    )
+    stats = prom.query_prometheus(query, timing[0], step=timing[1])
+    data['lines'].append({
+        'x': list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), stats[0])),
+        'y': stats[1],
+        'type': 'scatter',
+    })
+
+    data['layout'] = {
+        'yaxis': {
+            'ticksuffix': 'B',
+            'tickformat': '~s',
+            'range': [0, total_mem],
+        },
+        'showlegend': False,
+        'margin': {
+            'l': 70,
+            'r': 0,
+            'b': 50,
+            't': 0,
+            'pad': 0
+        },
+        'height': 300,
+    }
+
+    return JsonResponse(data)
+
+
+def graph_login_load(request, login):
+    if login not in settings.LOGINS.keys():
+        return JsonResponse({'error': 'Unknown login node'})
+    timing = query_time(request)
+    data = {'lines': []}
+
+    query = 'node_load1{{instance=~"{login}:.*", {filter} }}'.format(
+        login=login,
+        filter=prom.get_filter(),
+    )
+    stats = prom.query_prometheus(query, timing[0], step=timing[1])
+    data['lines'].append({
+        'x': list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), stats[0])),
+        'y': stats[1],
+        'type': 'scatter',
+    })
+
+    data['layout'] = {
+        'showlegend': False,
+        'margin': {
+            'l': 70,
+            'r': 0,
+            'b': 50,
+            't': 0,
+            'pad': 0
+        },
+        'height': 300,
+    }
+
+    return JsonResponse(data)
+
+
+def graph_login_network(request, login):
+    if login not in settings.LOGINS.keys():
+        return JsonResponse({'error': 'Unknown login node'})
+    device = settings.LOGINS[login]['network_interface']
+    timing = query_time(request)
+    data = {'lines': []}
+    query_rx = 'rate(node_network_receive_bytes_total{{instance=~"{login}:.*", device="{device}", {filter} }}[{step}]) * 8'.format(
+        login=login,
+        filter=prom.get_filter(),
+        device=device,
+        step=timing[1],
+    )
+    stats_rx = prom.query_prometheus(query_rx, timing[0], step=timing[1])
+
+    data['lines'].append({
+        'x': list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), stats_rx[0])),
+        'y': stats_rx[1],
+        'type': 'scatter',
+        'name': '{}'.format('Receive'),
+    })
+
+    query_tx = 'rate(node_network_transmit_bytes_total{{instance=~"{login}:.*", device="{device}", {filter} }}[{step}]) * 8'.format(
+        login=login,
+        filter=prom.get_filter(),
+        device=device,
+        step=timing[1],
+    )
+    stats_tx = prom.query_prometheus(query_tx, timing[0], step=timing[1])
+
+    data['lines'].append({
+        'x': list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), stats_tx[0])),
+        'y': stats_tx[1],
+        'type': 'scatter',
+        'name': '{}'.format('Transmit'),
+    })
+
+    data['layout'] = {
+        'yaxis': {
+            'ticksuffix': 'bps',
+            'tickformat': '~s',
+        },
+        'showlegend': False,
+        'margin': {
+            'l': 70,
+            'r': 0,
+            'b': 50,
+            't': 0,
+            'pad': 0
+        },
+        'height': 300,
+    }
+
     return JsonResponse(data)

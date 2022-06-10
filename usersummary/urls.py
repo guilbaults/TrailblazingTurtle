af9a631189db2bde42eb83b3a119bf7f3e41cad7
Simon Guilbault <sigui4@userportal.int.ets1.calculquebec.ca>
2022-06-10 18:02:46 +0000

    Adding quota graph in usersummary

--- usersummary/urls.py
+++ usersummary/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('<str:username>/', views.user),
+    path('<str:username>/graph/<str:fs>/inodes.json', views.graph_inodes),
+    path('<str:username>/graph/<str:fs>/bytes.json', views.graph_bytes),
 ]
--- usersummary/views.py
+++ usersummary/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from userportal.common import user_or_staff, Prometheus, username_to_uid
-from userportal.common import storage_allocations_project, storage_allocations_nearline
+from userportal.common import storage_allocations_project  # , storage_allocations_nearline
 from django.contrib.auth.decorators import login_required
+from django.utils.translation import gettext as _
 from slurm.models import JobTable
 from django.conf import settings
+from django.http import JsonResponse
+from datetime import datetime, timedelta
 
 prom = Prometheus(settings.PROMETHEUS)
 
@@ -13,25 +16,68 @@ def index(request):
     return redirect('{}/'.format(request.user.username.split('@')[0]))
 
 
-def get_quota_prometheus(quota_name, quota_type, inodes_bytes='inodes'):
+def get_quota_prometheus_request(quota_name, quota_type, inodes_bytes='inodes'):
     if inodes_bytes == 'inodes':
-        metric_name = 'lfs_quota_inodes'
-    else:
-        metric_name = 'lfs_quota_kbytes'
-
-    try:
-        query_quota = "{}{{path='{}', {}='{}', {} }}".format(
-            metric_name,
+        return "lfs_quota_inodes{{path='{}', {}='{}', {} }}".format(
             settings.QUOTA_TYPES[quota_type][0],
             settings.QUOTA_TYPES[quota_type][1],
             quota_name,
             prom.get_filter())
+    else:
+        return "lfs_quota_kbytes{{path='{}', {}='{}', {} }} * 1024".format(
+            settings.QUOTA_TYPES[quota_type][0],
+            settings.QUOTA_TYPES[quota_type][1],
+            quota_name,
+            prom.get_filter())
+
+
+def get_quota_prometheus(quota_name, quota_type, inodes_bytes='inodes'):
+    try:
+        query_quota = get_quota_prometheus_request(quota_name, quota_type, inodes_bytes)
         stats_quota = prom.query_last(query_quota)
-        if inodes_bytes == 'inodes':
-            return int(stats_quota[0]['value'][1])
-        else:
-            return int(stats_quota[0]['value'][1]) * 1024  # return it in bytes
+        return int(stats_quota[0]['value'][1])
     except IndexError:
+        return None
+
+
+def get_quota_scratch(username):
+    return {
+        'inodes_quota': 10000000,
+        'bytes_quota': 20 * 1024 * 1024 * 1024 * 1024,
+        'inodes': get_quota_prometheus(username, 'scratch', 'inodes'),
+        'bytes': get_quota_prometheus(username, 'scratch', 'bytes'),
+    }
+
+
+def get_quota_home(username):
+    return {
+        'inodes_quota': 500000,
+        'bytes_quota': 50 * 1024 * 1024 * 1024,
+        'inodes': get_quota_prometheus(username, 'home', 'inodes'),
+        'bytes': get_quota_prometheus(username, 'home', 'bytes'),
+    }
+
+
+def get_quota_projects(username):
+    context = {}
+    alloc_projects = storage_allocations_project(username)
+    # alloc_nearlines = storage_allocations_nearline(username)
+    for project in alloc_projects:
+        project['inodes'] = get_quota_prometheus(project['name'], 'project', 'inodes')
+        project['bytes'] = get_quota_prometheus(project['name'], 'project', 'bytes')
+    context['alloc_projects'] = alloc_projects
+    # context['alloc_nearlines'] = alloc_nearlines
+    return context
+
+
+def get_allocated_quota(username, fs):
+    if fs == 'scratch':
+        return get_quota_scratch(username)
+    elif fs == 'home':
+        return get_quota_home(username)
+    elif fs == 'project':
+        return get_quota_projects(username)
+    else:
         return None
 
 
@@ -42,26 +88,11 @@ def user(request, username):
     context = {'username': username}
 
     if 'lfs_quota' in settings.EXPORTER_INSTALLED:
-        context['scratch'] = {
-            'inodes_quota': 10000000,
-            'bytes_quota': 20 * 1024 * 1024 * 1024 * 1024,
-            'inodes': get_quota_prometheus(username, 'scratch', 'inodes'),
-            'bytes': get_quota_prometheus(username, 'scratch', 'bytes'),
-        }
-        context['home'] = {
-            'inodes_quota': 500000,
-            'bytes_quota': 50 * 1024 * 1024 * 1024,
-            'inodes': get_quota_prometheus(username, 'home', 'inodes'),
-            'bytes': get_quota_prometheus(username, 'home', 'bytes'),
-        }
-
-        alloc_projects = storage_allocations_project(username)
-        alloc_nearlines = storage_allocations_nearline(username)
-        for project in alloc_projects:
-            project['inodes'] = get_quota_prometheus(project['name'], 'project', 'inodes')
-            project['bytes'] = get_quota_prometheus(project['name'], 'project', 'bytes')
-        context['alloc_projects'] = alloc_projects
-        context['alloc_nearlines'] = alloc_nearlines
+        context['scratch'] = get_quota_scratch(username)
+        context['home'] = get_quota_home(username)
+        project = get_quota_projects(username)
+        context['alloc_projects'] = project['alloc_projects']
+        # context['alloc_nearlines'] = project['alloc_nearlines']
 
     if 'jobstats' in settings.INSTALLED_APPS:
         pending_jobs = JobTable.objects.filter(
@@ -85,3 +116,101 @@ def user(request, username):
             context['total_gpus'] += job.gpu_count()
 
     return render(request, 'usersummary/user.html', context)
+
+
+@login_required
+@user_or_staff
+def graph_inodes(request, username, fs):
+    project_name = request.GET.get('project_name')
+    if project_name is not None:
+        query = get_quota_prometheus_request(project_name, fs, 'inodes')
+        quota = get_allocated_quota(username, fs)
+        # got all the quotas for this user, now we need to filter by project
+        for project in quota['alloc_projects']:
+            if project['name'] == project_name:
+                quota = project
+    else:
+        query = get_quota_prometheus_request(username, fs, 'inodes')
+        quota = get_allocated_quota(username, fs)
+
+    stats = prom.query_prometheus_multiple(query, datetime.now() - timedelta(days=90), datetime.now(), step="6h")
+
+    data = {'lines': []}
+    for line in stats:
+        x = list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), line['x']))
+        y = line['y']
+        data['lines'].append({
+            'x': x,
+            'y': y,
+            'type': 'scatter',
+            'name': _('Used inodes'),
+        })
+
+    data['layout'] = {
+        'yaxis': {
+            'range': [0, max(quota['inodes_quota'], max(line['y']))],
+        },
+        'showlegend': False,
+        'margin': {
+            'l': 70,
+            'r': 10,
+            'b': 50,
+            't': 10,
+            'pad': 0
+        },
+        'height': 300,
+        'width': 300,
+    }
+
+    return JsonResponse(data)
+
+
+@login_required
+@user_or_staff
+def graph_bytes(request, username, fs):
+    project_name = request.GET.get('project_name')
+    if project_name is not None:
+        query = get_quota_prometheus_request(project_name, fs, 'bytes')
+        quota = get_allocated_quota(username, fs)
+        # got all the quotas for this user, now we need to filter by project
+        for project in quota['alloc_projects']:
+            if project['name'] == project_name:
+                quota = project
+        max_scale = quota['project_storage_bytes']  # somehow this is not the same key with project or user
+    else:
+        query = get_quota_prometheus_request(username, fs, 'bytes')
+        quota = get_allocated_quota(username, fs)
+        max_scale = quota['bytes_quota']
+
+    stats = prom.query_prometheus_multiple(query, datetime.now() - timedelta(days=180), datetime.now(), step="6h")
+
+    data = {'lines': []}
+    for line in stats:
+        x = list(map(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'), line['x']))
+        y = line['y']
+        data['lines'].append({
+            'x': x,
+            'y': y,
+            'type': 'scatter',
+            'name': _('Used bytes'),
+        })
+
+    data['layout'] = {
+        'yaxis': {
+            'range': [0, max(max_scale, max(line['y']))],
+            'ticksuffix': 'B',
+            'tickformat': '~s',
+        },
+        'showlegend': False,
+        'margin': {
+            'l': 70,
+            'r': 10,
+            'b': 50,
+            't': 10,
+            'pad': 0
+        },
+        'height': 300,
+        'width': 300,
+    }
+
+    return JsonResponse(data)

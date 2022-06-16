35c49723a84360d53e7e260b31ce88a0c14dcaf0
Simon Guilbault <sigui4@userportal101.narval.calcul.quebec>
2022-06-16 18:29:25 +0000

    Moving step argument into the url so it does not breaker other graphs

--- static/custom.js
+++ static/custom.js
@@ -6,12 +6,12 @@ function replace_div_nodata(container_div){
     $(container_div).html('<div class="alert alert-primary" role="alert">No data</div>');
 }
 
-function loadGraph(container, url, step = '3m'){
+function loadGraph(container, url){
     var container_div = '#' + container;
     $(container_div).html('<div class="spinner-border m-5 justify-content-center" role="status"><span class="sr-only">Loading...</span></div>');
 
     $.ajax({
-        url : url + '?step=' + step,
+        url : url,
         type : 'GET',
         tryCount : 0,
         retryLimit : 3,

5cdbba119798528288bc6b7ce0337ac0183bccd1
Simon Guilbault <simon.guilbault@calculquebec.ca>
2022-10-06 17:37:52 +0000

    Collect static files and generate translation when building the image

--- run-django-production.sh
+++ run-django-production.sh
@@ -5,6 +5,6 @@ set -o pipefail
 set -o nounset
 
 cp /secrets/settings/* /opt/userportal/userportal/settings.py
-mkdir -p /var/www/api/
-python3 manage.py collectstatic --noinput
+mkdir -p /var/www/api/static
+cp -r /opt/userportal/collected-static/* /var/www/api/static/
 gunicorn --bind :8000 --workers 1 --timeout 90 userportal.wsgi

4adedacfbbd45702060e038da1744d94887be5fe
Simon Guilbault <simon.guilbault@calculquebec.ca>
2022-10-06 14:35:18 +0000

    Health checks with watchman

--- run-django-production.sh
+++ run-django-production.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+cp /secrets/settings/* /opt/userportal/userportal/settings.py
 mkdir -p /var/www/api/
 python3 manage.py collectstatic --noinput
 gunicorn --bind :8000 --workers 1 --timeout 90 userportal.wsgi

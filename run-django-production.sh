9751f1d24882484bb2719c4bdb669b64608b513b
Simon Guilbault <simon.guilbault@calculquebec.ca>
2022-11-03 15:06:25 -0400

    Building containers on GitHub and storing them in ghcr.io (#6)

--- run-django-production.sh
+++ run-django-production.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-cp /secrets/settings/* /opt/userportal/userportal/settings.py
+cp /secrets/settings/99-local.py /opt/userportal/userportal/settings/99-local.py
 mkdir -p /var/www/api/static
 cp -r /opt/userportal/collected-static/* /var/www/api/static/
 gunicorn --bind :8000 --workers 1 --timeout 90 userportal.wsgi

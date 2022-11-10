0429294faeb79967aad2a36c4ef74fb7c23fa1b4
Simon Guilbault <simon.guilbault@calculquebec.ca>
2022-11-10 19:54:07 +0000

    Adding tests for accountstats module

--- userportal/settings/90-tests.py
+++ userportal/settings/90-tests.py
@@ -5,3 +5,6 @@ TESTS_ADMIN = 'admin'
 
 # a list of interesting users and jobs to test
 TESTS_JOBSTATS = [('user01', 1)]
+
+# a list of interesting slurm accounts to test in accountstats
+TESTS_ACCOUNTS = [('user01', 'def-user01_cpu', 'cpu')]

beb2974cc1211f428593bdd7804abee9b35158d3
Evan Vokey <49615340+Evan-Vokey@users.noreply.github.com>
2023-08-29 14:18:52 +0000

    remove unused tests_backends

--- cfaccess/tests_backends.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from cfaccess.backends import CloudflareAccessLDAPBackend
-
-class CloudflareAccessLDAPBackendTestCase(TestCase):
-    def setUp(self):
-        pass
-
-    def test_clean_username(self):
-        

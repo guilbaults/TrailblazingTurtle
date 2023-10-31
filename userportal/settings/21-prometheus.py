a53fd7f486aecafe1a24c709a53c2756f69d12b7
Simon Guilbault <simon.guilbault@calculquebec.ca>
2023-10-31 18:22:05 +0000

    Adding a setting for custom prometheus filter per module, used with cloudstats

--- userportal/settings/21-prometheus.py
+++ userportal/settings/21-prometheus.py
@@ -1,7 +1,10 @@
 PROMETHEUS = {
     'url': 'https://thanos.dant.computecanada.ca',
     'headers': {'Authorization': 'Basic changeme_base64'},
-    'filter': "cluster='narval'",
+    'filter': {
+        'default': "cluster='narval'",
+        'cloudstats': "cluster='narval', instance=~'blg.*'" # example to override the default filter for a specific module
+    },
 }
 
 PROM_NODE_HOSTNAME_LABEL = 'instance'

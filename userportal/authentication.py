ea20c5379fc03b2fb3b90094efe024a6f7b6c649
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2026-04-09 15:00:13 -0400

    make staff and access attributes configurable for saml2

--- userportal/authentication.py
+++ userportal/authentication.py
@@ -1,15 +1,25 @@
 from djangosaml2.backends import Saml2Backend
 from django.contrib.auth.backends import RemoteUserBackend
 
+from django.conf import settings
+
 
 class staffSaml2Backend(Saml2Backend):
     """This will add/remove the is_staff attribute from the user as appropriate."""
     def _update_user(self, user, attributes: dict, attribute_mapping: dict, force_save: bool = False):
-        if 'eduPersonAffiliation' in attributes:
-            if 'staff' in attributes['eduPersonAffiliation']:
+        # figure out if user is active (i.e. can login)
+        user.is_active = True
+        for attribute, value in settings.SAML_CONFIG['required_access_attributes']:
+            if attribute not in attributes or value not in attributes[attribute]:
+                user.is_active = False
+
+        # figure out if user is staff
+        user.is_staff = False
+        for attribute, value in settings.SAML_CONFIG['staff_attributes']:
+            if attribute in attributes and value in attributes[attribute]:
                 user.is_staff = True
-            else:
-                user.is_staff = False
+                break
+
         user.first_name = attributes['givenName'][0]
         user.last_name = attributes['sn'][0]
         force_save = True
